46420b165f2696cd6f128987c75788954fef2244
BrentRevelle <revellebrent@icloud.com>
2025-06-02 17:02:33 -0500

    update modal css

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,4 +1,5 @@
 import "./ModalWithForm.css";
+import closeIcon from "../assets/close-icon.svg";
 
 function ModalWithForm() {
    return (
@@ -29,23 +30,23 @@ function ModalWithForm() {
          </label>
          <fieldset className="modal__radio-buttons">
           <legend className="modal__legend">Select the weather type:</legend>
-          <label htmlFor="hot" className="modal__label modal__label_type_radio"> Hot
+          <label htmlFor="hot" className="modal__label modal__label_type_radio">
             <input id="hot"
              type="radio"
              className="modal__radio-input"
-             />
+             />Hot
           </label>
-          <label htmlFor="warm" className="modal__label modal__label_type_radio"> Warm
+          <label htmlFor="warm" className="modal__label modal__label_type_radio">
             <input id="warm"
              type="radio"
              className="modal__radio-input"
-             />
+             />Warm
           </label>
-          <label htmlFor="cold" className="modal__label modal__label_type_radio"> Cold
+          <label htmlFor="cold" className="modal__label modal__label_type_radio">
             <input id="cold"
              type="radio"
              className="modal__radio-input"
-             />
+             />Cold
           </label>
          </fieldset>
          <button type="submit" className="modal__submit">
