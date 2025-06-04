dfd8e9053d13f37d752e11f82f137380ed80138a
BrentRevelle <revellebrent@icloud.com>
2025-06-04 18:25:34 -0500

    API interactions done, dynamic WeatherCard

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,10 +1,34 @@
 import "./WeatherCard.css";
-import sunny from "../../assets/sunny.png";
+import { weatherOptions, defaultWeatherOptions } from "../../utils/constants";
 
 function WeatherCard({ weatherData }) {
+  const filteredOptions = weatherOptions.filter((option) => {
+    return (
+    option.day === weatherData.isDay &&
+    option.condition === weatherData.condition
+    );
+  });
+
+  let weatherOption;
+  let isFallback = false;
+
+  if (filteredOptions.length === 0) {
+    weatherOption = defaultWeatherOptions[weatherData.isDay ? "day" : "night"];
+    isFallback = true;
+  } else {
+    weatherOption = filteredOptions[0];
+  }
+
+  const altText = isFallback
+  ? `It is ${weatherData.isDay ? "daytime" : "nighttime"}`
+  : `Card showing ${weatherData.isDay ? "day" : "night"}time ${weatherOption.condition} weather`;
+
   return <section className="weather-card">
     <p className="weather-card__temp">{weatherData.temp.F} &deg;F</p>
-    <img src={sunny} alt="sunny" className="weather-card__image" />
+    <img
+    src={weatherOption?.url}
+    alt={altText}
+    className="weather-card__image" />
   </section>;
 }
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,75 @@
+export const weatherOptions = [
+  {
+    day: true,
+    condition: "Clear",
+    url: new URL("../assets/day/clear.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "Clouds",
+    url: new URL("../assets/day/cloudy.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "Mist",
+    url: new URL("../assets/day/foggy.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "Rain",
+    url: new URL("../assets/day/rain.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "Snow",
+    url: new URL("../assets/day/snow.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "Thunderstorm",
+    url: new URL("../assets/day/storm.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "Clear",
+    url: new URL("../assets/night/clear.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "Clouds",
+    url: new URL("../assets/night/cloudy.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "Mist",
+    url: new URL("../assets/night/foggy.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "Rain",
+    url: new URL("../assets/night/rain.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "Snow",
+    url: new URL("../assets/night/snow.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "Thunderstorm",
+    url: new URL("../assets/night/storm.png", import.meta.url).href,
+  },
+];
+
+export const defaultWeatherOptions = {
+  day: {
+    url: new URL("../assets/day/fallback.png", import.meta.url).href,
+  },
+  night: {
+    url: new URL ("../assets/night/fallback.png", import.meta.url).href,
+  },
+};
+
 export const defaultClothingItems = [
   {
     _id: 0,
